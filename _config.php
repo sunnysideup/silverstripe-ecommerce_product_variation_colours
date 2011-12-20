39863696fc9672f7bfee724d3e4a95c428aaf26a
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-12-20 07:57:40 +0000

    API: introducing colour extension for variations (this was previously part of the product variation module - and will now be separated).
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_variation_colours/trunk@1959 950b781c-c544-efab-2f99-90083ee69683

---
+++ _config.php
@@ -0,0 +1,13 @@
+<?php
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+// __________________________________ START ECOMMERCE PRODUCT VARIATIONS COLOUR MODULE CONFIG __________________________________
+//MUST SET
+//Object::add_extension("ProductAttributeValue", "ProductAttributeDecoratorColour_Value");
+//Object::add_extension("ProductAttributeType", "ProductAttributeDecoratorColour_Type");
+//MAY SET
+//ProductAttributeDecoratorColour_Value::set_default_contrast_colour("FFFFFF");
+//ProductAttributeDecoratorColour_Value::set_default_colour("000000");
+//ProductAttributeDecoratorColour_Value::set_put_styling_in_dropdown_options(true);
+//
+// __________________________________ END ECOMMERCE PRODUCT VARIATIONS COLOUR MODULE CONFIG __________________________________
